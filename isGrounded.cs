1158a19f8ed393ff6488b044d6f24a9e8e671765
bansheerubber <gary.cactus.email@gmail.com>
2020-06-13 19:02:28 -0700

    changed is grounded

--- isGrounded.cs
+++ isGrounded.cs
@@ -8,6 +8,6 @@ package IsGroundedPackage {
 activatePackage(IsGroundedPackage);
 
 function Player::isGroundedLoop(%this) {
-	%this.isGrounded = !containerBoxEmpty($TypeMasks::fxBrickObjectType | $TypeMasks::StaticObjectType, %this.getPosition(), 0.6, 0.6, 0.3);
+	%this.isGrounded = !containerBoxEmpty($TypeMasks::fxBrickObjectType | $TypeMasks::StaticObjectType, %this.getPosition(), 0.6, 0.6, 0.6);
 	%this.isGroundedSchedule = %this.schedule(100, isGroundedLoop);
 }
